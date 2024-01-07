995e261acf36bbc09abfa63120cc9bd565ebff74
Gleb Bashkatov <glebbash@gmail.com>
2024-01-07 21:57:57 +0000

    feat: add snapping to grid

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import "@pixi/math-extras";
 import * as PIXI from "pixi.js";
 import { Viewport } from "pixi-viewport";
 
-// TODO: save pieces state (also zoom and pan?)
 // TODO: add piece rotation
 // TODO: add piece flipping
-// TODO: add piece elevation
-// TODO: add piece snapping (inside box only + check overlaps)
-// TODO: add yjs rooms
-// TODO: add piece ball connectors?
+// TODO: add yjs rooms + local storage
+// TODO: do not dorp piece if overlaps
+// TODO?: add piece ball connectors
+// TODO?: snap inside box only
 
 const WORLD_SIZE = 2000;
 const DENT_RADIUS = 13;
@@ -16,6 +15,11 @@ const BALL_PIECE_RADIUS = DENT_RADIUS * 1.125;
 const REFLECTION_RADIUS = DENT_RADIUS / 7;
 const BOARD_MAX_X = 11;
 const BOARD_MAX_Y = 5;
+const DIF = BALL_PIECE_RADIUS - DENT_RADIUS;
+const BOARD_MARGIN = new PIXI.Point(
+  DENT_RADIUS * 0.5 - DIF,
+  DENT_RADIUS * 0.5 - DIF
+);
 
 let maxZIndex = 100;
 let dragTarget: PIXI.Container | undefined;
@@ -85,103 +89,6 @@ function main() {
     }
   }
 
-  const DIF = BALL_PIECE_RADIUS - DENT_RADIUS;
-  const padding = new PIXI.Point(
-    DENT_RADIUS * 0.5 - DIF,
-    DENT_RADIUS * 0.5 - DIF
-  );
-
-  const magentaPiece = puzzleBox.addChild(new PIXI.Container());
-  magentaPiece.addChild(makeBall("#e66ba2")).position = gridToWorld(0, 0);
-  magentaPiece.addChild(makeBall("#e66ba2")).position = gridToWorld(1, 0);
-  magentaPiece.addChild(makeBall("#e66ba2")).position = gridToWorld(2, 0);
-  magentaPiece.addChild(makeBall("#e66ba2")).position = gridToWorld(2, 1);
-  magentaPiece.addChild(makeBall("#e66ba2")).position = gridToWorld(3, 1);
-  magentaPiece.position = gridToWorld(0, 0).add(padding);
-
-  const tealPiece = puzzleBox.addChild(new PIXI.Container());
-  tealPiece.addChild(makeBall("#83c2b9")).position = gridToWorld(0, 0);
-  tealPiece.addChild(makeBall("#83c2b9")).position = gridToWorld(0, 1);
-  tealPiece.addChild(makeBall("#83c2b9")).position = gridToWorld(1, 0);
-  tealPiece.addChild(makeBall("#83c2b9")).position = gridToWorld(1, 1);
-  tealPiece.addChild(makeBall("#83c2b9")).position = gridToWorld(1, 2);
-  tealPiece.position = gridToWorld(0, 1).add(padding);
-
-  const lightGreenPiece = puzzleBox.addChild(new PIXI.Container());
-  lightGreenPiece.addChild(makeBall("#8eb71d")).position = gridToWorld(0, 0);
-  lightGreenPiece.addChild(makeBall("#8eb71d")).position = gridToWorld(0, 1);
-  lightGreenPiece.addChild(makeBall("#8eb71d")).position = gridToWorld(1, 1);
-  lightGreenPiece.addChild(makeBall("#8eb71d")).position = gridToWorld(2, 1);
-  lightGreenPiece.addChild(makeBall("#8eb71d")).position = gridToWorld(2, 0);
-  lightGreenPiece.position = gridToWorld(0, 3).add(padding);
-
-  const maroonPiece = puzzleBox.addChild(new PIXI.Container());
-  maroonPiece.addChild(makeBall("#aa201e")).position = gridToWorld(0, 1);
-  maroonPiece.addChild(makeBall("#aa201e")).position = gridToWorld(1, 1);
-  maroonPiece.addChild(makeBall("#aa201e")).position = gridToWorld(1, 0);
-  maroonPiece.addChild(makeBall("#aa201e")).position = gridToWorld(2, 0);
-  maroonPiece.position = gridToWorld(3, 3).add(padding);
-
-  const orangePiece = puzzleBox.addChild(new PIXI.Container());
-  orangePiece.addChild(makeBall("#e78a00")).position = gridToWorld(0, 1);
-  orangePiece.addChild(makeBall("#e78a00")).position = gridToWorld(1, 1);
-  orangePiece.addChild(makeBall("#e78a00")).position = gridToWorld(2, 1);
-  orangePiece.addChild(makeBall("#e78a00")).position = gridToWorld(2, 0);
-  orangePiece.addChild(makeBall("#e78a00")).position = gridToWorld(1, 2);
-  orangePiece.position = gridToWorld(2, 1).add(padding);
-
-  const darkGreenPiece = puzzleBox.addChild(new PIXI.Container());
-  darkGreenPiece.addChild(makeBall("#008455")).position = gridToWorld(0, 1);
-  darkGreenPiece.addChild(makeBall("#008455")).position = gridToWorld(1, 1);
-  darkGreenPiece.addChild(makeBall("#008455")).position = gridToWorld(2, 1);
-  darkGreenPiece.addChild(makeBall("#008455")).position = gridToWorld(1, 0);
-  darkGreenPiece.position = gridToWorld(5, 3).add(padding);
-
-  const purplePiece = puzzleBox.addChild(new PIXI.Container());
-  purplePiece.addChild(makeBall("#7b1d7b")).position = gridToWorld(0, 0);
-  purplePiece.addChild(makeBall("#7b1d7b")).position = gridToWorld(1, 0);
-  purplePiece.addChild(makeBall("#7b1d7b")).position = gridToWorld(1, 1);
-  purplePiece.addChild(makeBall("#7b1d7b")).position = gridToWorld(2, 1);
-  purplePiece.addChild(makeBall("#7b1d7b")).position = gridToWorld(2, 2);
-  purplePiece.position = gridToWorld(6, 2).add(padding);
-
-  const darkBluePiece = puzzleBox.addChild(new PIXI.Container());
-  darkBluePiece.addChild(makeBall("#143d8c")).position = gridToWorld(0, 0);
-  darkBluePiece.addChild(makeBall("#143d8c")).position = gridToWorld(1, 0);
-  darkBluePiece.addChild(makeBall("#143d8c")).position = gridToWorld(1, 1);
-  darkBluePiece.addChild(makeBall("#143d8c")).position = gridToWorld(1, 2);
-  darkBluePiece.position = gridToWorld(8, 2).add(padding);
-
-  const redPiece = puzzleBox.addChild(new PIXI.Container());
-  redPiece.addChild(makeBall("#d30710")).position = gridToWorld(0, 0);
-  redPiece.addChild(makeBall("#d30710")).position = gridToWorld(1, 0);
-  redPiece.addChild(makeBall("#d30710")).position = gridToWorld(1, 1);
-  redPiece.addChild(makeBall("#d30710")).position = gridToWorld(1, 2);
-  redPiece.addChild(makeBall("#d30710")).position = gridToWorld(1, 3);
-  redPiece.position = gridToWorld(9, 1).add(padding);
-
-  const yellowPiece = puzzleBox.addChild(new PIXI.Container());
-  yellowPiece.addChild(makeBall("#f5d100")).position = gridToWorld(0, 0);
-  yellowPiece.addChild(makeBall("#f5d100")).position = gridToWorld(1, 0);
-  yellowPiece.addChild(makeBall("#f5d100")).position = gridToWorld(2, 0);
-  yellowPiece.addChild(makeBall("#f5d100")).position = gridToWorld(3, 0);
-  yellowPiece.addChild(makeBall("#f5d100")).position = gridToWorld(1, 1);
-  yellowPiece.position = gridToWorld(7, 0).add(padding);
-
-  const bluePiece = puzzleBox.addChild(new PIXI.Container());
-  bluePiece.addChild(makeBall("#0094d4")).position = gridToWorld(0, 0);
-  bluePiece.addChild(makeBall("#0094d4")).position = gridToWorld(1, 0);
-  bluePiece.addChild(makeBall("#0094d4")).position = gridToWorld(2, 0);
-  bluePiece.addChild(makeBall("#0094d4")).position = gridToWorld(2, 1);
-  bluePiece.addChild(makeBall("#0094d4")).position = gridToWorld(2, 2);
-  bluePiece.position = gridToWorld(3, 0).add(padding);
-
-  const lightBluePiece = puzzleBox.addChild(new PIXI.Container());
-  lightBluePiece.addChild(makeBall("#98ceea")).position = gridToWorld(0, 0);
-  lightBluePiece.addChild(makeBall("#98ceea")).position = gridToWorld(0, 1);
-  lightBluePiece.addChild(makeBall("#98ceea")).position = gridToWorld(1, 1);
-  lightBluePiece.position = gridToWorld(6, 0).add(padding);
-
   app.stage.eventMode = "static";
   app.stage.hitArea = app.screen;
   app.stage.on("pointermove", (event) => {
@@ -197,12 +104,115 @@ function main() {
     if (!dragTarget) return;
 
     dragTarget.alpha = 1;
+    const gridPos = worldToGrid(dragTarget.position);
+    dragTarget.position = gridToWorld(gridPos.x, gridPos.y);
     dragTarget = undefined;
   }
+
+  const board = puzzleBox.addChild(new PIXI.Container());
+  board.position = BOARD_MARGIN;
+
+  const magentaPiece = board.addChild(new PIXI.Container());
+  magentaPiece.addChild(makeBall("#e66ba2")).position = gridToWorld(0, 0);
+  magentaPiece.addChild(makeBall("#e66ba2")).position = gridToWorld(1, 0);
+  magentaPiece.addChild(makeBall("#e66ba2")).position = gridToWorld(2, 0);
+  magentaPiece.addChild(makeBall("#e66ba2")).position = gridToWorld(2, 1);
+  magentaPiece.addChild(makeBall("#e66ba2")).position = gridToWorld(3, 1);
+  magentaPiece.position = gridToWorld(0, 0);
+
+  const tealPiece = board.addChild(new PIXI.Container());
+  tealPiece.addChild(makeBall("#83c2b9")).position = gridToWorld(0, 0);
+  tealPiece.addChild(makeBall("#83c2b9")).position = gridToWorld(0, 1);
+  tealPiece.addChild(makeBall("#83c2b9")).position = gridToWorld(1, 0);
+  tealPiece.addChild(makeBall("#83c2b9")).position = gridToWorld(1, 1);
+  tealPiece.addChild(makeBall("#83c2b9")).position = gridToWorld(1, 2);
+  tealPiece.position = gridToWorld(0, 1);
+
+  const lightGreenPiece = board.addChild(new PIXI.Container());
+  lightGreenPiece.addChild(makeBall("#8eb71d")).position = gridToWorld(0, 0);
+  lightGreenPiece.addChild(makeBall("#8eb71d")).position = gridToWorld(0, 1);
+  lightGreenPiece.addChild(makeBall("#8eb71d")).position = gridToWorld(1, 1);
+  lightGreenPiece.addChild(makeBall("#8eb71d")).position = gridToWorld(2, 1);
+  lightGreenPiece.addChild(makeBall("#8eb71d")).position = gridToWorld(2, 0);
+  lightGreenPiece.position = gridToWorld(0, 3);
+
+  const maroonPiece = board.addChild(new PIXI.Container());
+  maroonPiece.addChild(makeBall("#aa201e")).position = gridToWorld(0, 1);
+  maroonPiece.addChild(makeBall("#aa201e")).position = gridToWorld(1, 1);
+  maroonPiece.addChild(makeBall("#aa201e")).position = gridToWorld(1, 0);
+  maroonPiece.addChild(makeBall("#aa201e")).position = gridToWorld(2, 0);
+  maroonPiece.position = gridToWorld(3, 3);
+
+  const orangePiece = board.addChild(new PIXI.Container());
+  orangePiece.addChild(makeBall("#e78a00")).position = gridToWorld(0, 1);
+  orangePiece.addChild(makeBall("#e78a00")).position = gridToWorld(1, 1);
+  orangePiece.addChild(makeBall("#e78a00")).position = gridToWorld(2, 1);
+  orangePiece.addChild(makeBall("#e78a00")).position = gridToWorld(2, 0);
+  orangePiece.addChild(makeBall("#e78a00")).position = gridToWorld(1, 2);
+  orangePiece.position = gridToWorld(2, 1);
+
+  const darkGreenPiece = board.addChild(new PIXI.Container());
+  darkGreenPiece.addChild(makeBall("#008455")).position = gridToWorld(0, 1);
+  darkGreenPiece.addChild(makeBall("#008455")).position = gridToWorld(1, 1);
+  darkGreenPiece.addChild(makeBall("#008455")).position = gridToWorld(2, 1);
+  darkGreenPiece.addChild(makeBall("#008455")).position = gridToWorld(1, 0);
+  darkGreenPiece.position = gridToWorld(5, 3);
+
+  const purplePiece = board.addChild(new PIXI.Container());
+  purplePiece.addChild(makeBall("#7b1d7b")).position = gridToWorld(0, 0);
+  purplePiece.addChild(makeBall("#7b1d7b")).position = gridToWorld(1, 0);
+  purplePiece.addChild(makeBall("#7b1d7b")).position = gridToWorld(1, 1);
+  purplePiece.addChild(makeBall("#7b1d7b")).position = gridToWorld(2, 1);
+  purplePiece.addChild(makeBall("#7b1d7b")).position = gridToWorld(2, 2);
+  purplePiece.position = gridToWorld(6, 2);
+
+  const darkBluePiece = board.addChild(new PIXI.Container());
+  darkBluePiece.addChild(makeBall("#143d8c")).position = gridToWorld(0, 0);
+  darkBluePiece.addChild(makeBall("#143d8c")).position = gridToWorld(1, 0);
+  darkBluePiece.addChild(makeBall("#143d8c")).position = gridToWorld(1, 1);
+  darkBluePiece.addChild(makeBall("#143d8c")).position = gridToWorld(1, 2);
+  darkBluePiece.position = gridToWorld(8, 2);
+
+  const redPiece = board.addChild(new PIXI.Container());
+  redPiece.addChild(makeBall("#d30710")).position = gridToWorld(0, 0);
+  redPiece.addChild(makeBall("#d30710")).position = gridToWorld(1, 0);
+  redPiece.addChild(makeBall("#d30710")).position = gridToWorld(1, 1);
+  redPiece.addChild(makeBall("#d30710")).position = gridToWorld(1, 2);
+  redPiece.addChild(makeBall("#d30710")).position = gridToWorld(1, 3);
+  redPiece.position = gridToWorld(9, 1);
+
+  const yellowPiece = board.addChild(new PIXI.Container());
+  yellowPiece.addChild(makeBall("#f5d100")).position = gridToWorld(0, 0);
+  yellowPiece.addChild(makeBall("#f5d100")).position = gridToWorld(1, 0);
+  yellowPiece.addChild(makeBall("#f5d100")).position = gridToWorld(2, 0);
+  yellowPiece.addChild(makeBall("#f5d100")).position = gridToWorld(3, 0);
+  yellowPiece.addChild(makeBall("#f5d100")).position = gridToWorld(1, 1);
+  yellowPiece.position = gridToWorld(7, 0);
+
+  const bluePiece = board.addChild(new PIXI.Container());
+  bluePiece.addChild(makeBall("#0094d4")).position = gridToWorld(0, 0);
+  bluePiece.addChild(makeBall("#0094d4")).position = gridToWorld(1, 0);
+  bluePiece.addChild(makeBall("#0094d4")).position = gridToWorld(2, 0);
+  bluePiece.addChild(makeBall("#0094d4")).position = gridToWorld(2, 1);
+  bluePiece.addChild(makeBall("#0094d4")).position = gridToWorld(2, 2);
+  bluePiece.position = gridToWorld(3, 0);
+
+  const lightBluePiece = board.addChild(new PIXI.Container());
+  lightBluePiece.addChild(makeBall("#98ceea")).position = gridToWorld(0, 0);
+  lightBluePiece.addChild(makeBall("#98ceea")).position = gridToWorld(0, 1);
+  lightBluePiece.addChild(makeBall("#98ceea")).position = gridToWorld(1, 1);
+  lightBluePiece.position = gridToWorld(6, 0);
 }
 
 function gridToWorld(x: number, y: number) {
   return new PIXI.Point(DENT_RADIUS * 2.5 * x, DENT_RADIUS * 2.5 * y);
+}
+
+function worldToGrid(pos: PIXI.Point) {
+  return new PIXI.Point(
+    Math.round(pos.x / (DENT_RADIUS * 2.5)),
+    Math.round(pos.y / (DENT_RADIUS * 2.5))
+  );
 }
 
 function makeBall(color: PIXI.ColorSource) {
